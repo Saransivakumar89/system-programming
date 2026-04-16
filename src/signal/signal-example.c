6998632150c888773f43b66603f56faa23aa7734
Saransivakumar89 <saransivakumar89@gmail.com>
2026-04-16 21:49:05 +0530

    feat(sig): add the SIG_IGN + SIG_DFL

--- src/signal/signal-example.c
+++ src/signal/signal-example.c
@@ -7,6 +7,10 @@
 /*
  * When the ctrl + c , SIGINT is sent
  * handler starts exexcutes instead of the terminating the program
+ * 
+ * custom handler catches the SIGINT
+ * SIG_IGN ignores the SIGINT
+ * SIG_DFL terminates the SIGINT
  * */
 
 void sig_handler(int sig){
@@ -17,7 +21,17 @@ void sig_handler(int sig){
 
 int main(void){
     
+    printf("Custom handler(5 secs)\n");
     signal(SIGINT, sig_handler);    // Handles the CTRL + C
+    sleep(5);
+    
+    printf("\nignores the SIGINT (5 secs)\n");
+    signal(SIGINT, SIG_IGN);    // ignores the CTRL + C
+    sleep(5); 
+
+    printf("\nDefault handler (5 secs)\n");
+    signal(SIGINT, SIG_DFL);    // Default Handler for CTRL + C(terminates)
+    sleep(5);
 
     while(1){
         printf("running...\n");
