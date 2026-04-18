49ddac4d3d82819e4f2e849fef9766ac6a355bb8
Saransivakumar89 <saransivakumar89@gmail.com>
2026-04-18 13:07:14 +0530

    feat(sig): add the multiple signals

--- src/signal/sigaction-example.c
+++ src/signal/sigaction-example.c
@@ -4,18 +4,23 @@
 #include<signal.h>
 #include<fcntl.h>
 #include<setjmp.h>
+#include<sys/wait.h>
 
 sigjmp_buf env;
 
 void sig_handler(int sig){  //Custom handler
     
     if(sig == SIGINT){
-        printf("\nCaught SIGINT (ctrl+c)\n");
+        printf("\nCaught SIGINT (ctrl+c)");
     }else if(sig == SIGTERM){
-        printf("\nCaught SIGTERM\n");
+        printf("\nCaught SIGTERM");
     }else if(sig == SIGFPE){
-        printf("\nCaught SIGFPE\n");
+        printf("\nCaught SIGFPE");
         siglongjmp(env,1);          //jumpout safely
+    }else if(sig == SIGCHLD){
+        printf("\nCaught SIGCHLD");
+    }else if(sig == SIGALRM){
+        printf("\nCaught SIGALRM");
     }
 
 }
@@ -49,6 +54,18 @@ int main(void){
         return -1;
     }
     
+    //Register handler for SIGCHLD
+    if(sigaction(SIGCHLD, &sa, NULL)==-1){
+        perror("sigaction");
+        return -1;
+    }
+
+    //REgister handler for SIGALRM
+    if(sigaction(SIGALRM,&sa,NULL)==-1){
+        perror("sigaction");
+        return -1;
+    }
+    
 
     sleep(1);
 
@@ -57,6 +74,28 @@ int main(void){
         a = a / b;          //Fault occurs
     }
           
+    sleep(1);
+    fflush(stdout);
+    pid_t pid = fork();
+
+    if(pid < 0){
+        // fork failed
+        perror("fork failed");
+        return -1;
+    }
+    else if(pid == 0){ //child process
+        exit(0);
+    }
+    else if(pid > 0){   //parent process
+        wait(NULL);
+    }
+
+    sleep(1);
+
+    alarm(2);   //timer for 2 secs
+    
+    sleep(1);
+
     printf("\nPress ctrl + c or send TERM to %d\n", getpid());
 
     while(1){
