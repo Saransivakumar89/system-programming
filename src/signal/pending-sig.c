23371e50e24de5bd2747676cf73f59673617728e
Saransivakumar89 <saransivakumar89@gmail.com>
2026-04-19 19:41:39 +0530

    feat(sig): understood the sigpending

--- src/signal/pending-sig.c
+++ src/signal/pending-sig.c
@@ -7,25 +7,35 @@
 
 int main(void){
     
-    sigset_t set;
+    sigset_t set, pending;
 
     sigemptyset(&set);
+    sigemptyset(&pending);
+    
     sigaddset(&set, SIGINT);
     sigprocmask(SIG_BLOCK, &set, NULL);
 
-    printf("press ctrl+C\n");
+    printf("press ctrl+C...\n");
     sleep(5);
     sigprocmask(SIG_BLOCK, &set, NULL);
 
-    sigpending(&set);   //check the pending signal
+    sigpending(&pending);   //check the pending signal
     
-    if(sigismember(&set,SIGINT)){
-        printf("SIGINT is pending\n");
+    if(sigismember(&pending,SIGINT)){
+        printf("SIGINT Pending\n");
     }
     else {
-        printf("SIGINT is not pending\n");
+        printf("SIGINT not pending\n");
     }
-
+    
+    //unblock
+    sigprocmask(SIG_UNBLOCK, &set, NULL);
+    
+    while(1){
+        printf("...\n");
+    	pause();
+    }
+    
 
     return 0 ;
 }
