22bb6a42e9f46ae365b4258da04d5f99491fea27
Saransivakumar89 <saransivakumar89@gmail.com>
2026-04-23 08:37:37 +0530

    feat(mkfifo): add the multi-reader

---
+++ src/pipe/2-reader.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define FIFO_PATH "/tmp/myfifo"
+
+int main() {
+    int fd;
+    char buffer[100];
+
+    // Create FIFO (ignore error if it already exists)
+    /*if (mkfifo(FIFO_PATH, 0666) == -1) {
+        perror("mkfifo");
+    }*/
+
+    fd = open(FIFO_PATH, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    read(fd, buffer, sizeof(buffer));
+    printf("2nd- reader Received: %s\n", buffer);
+
+    close(fd);
+    return 0;
+}
