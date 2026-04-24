a3e2f2a93a619f59675854bb118911be5356ea28
Saransivakumar89 <saransivakumar89@gmail.com>
2026-04-24 22:50:26 +0530

    feat(select): implement the 2 child + stdin

--- src/multiplex-io/pipe-stdin.c
+++ src/multiplex-io/pipe-stdin.c
@@ -6,41 +6,48 @@
 
 int main(void) {
     fd_set readfds;
-    int fd[2];
+    int fd1[2], fd2[2];
     char buff[100];
 
-    if (pipe(fd) < 0) {
+    if (pipe(fd1) < 0 || pipe(fd2) < 0) {
         perror("pipe");
         exit(1);
     }
 
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(1);
-    }
-
-    if (pid == 0) {
-        close(fd[0]);
+    if (fork() == 0) {
+        close(fd1[0]);
+        close(fd2[0]);
+        close(fd2[1]);
 
         while (1) {
-            const char *msg = "Message from child\n";
-            write(fd[1], msg, strlen(msg));
+            const char *msg = "PING\n";
+            write(fd1[1], msg, strlen(msg));
             sleep(3);
         }
     }
 
+    if (fork() == 0) {
+        close(fd2[0]);
+        close(fd1[0]);
+        close(fd1[1]);
 
-    close(fd[1]);
+        while (1) {
+            const char *msg = "PONG\n";
+            write(fd2[1], msg, strlen(msg));
+            sleep(3);
+        }
+    }
+
+    close(fd1[1]);
+    close(fd2[1]);
 
     while (1) {
         FD_ZERO(&readfds);
         FD_SET(STDIN_FILENO, &readfds);
-        FD_SET(fd[0], &readfds);
+        FD_SET(fd1[0], &readfds);
+        FD_SET(fd2[0], &readfds);
 
-        int maxfd = fd[0] ;
-
-        printf("waiting for stdin or pipe...\n");
+        int maxfd = fd1[0] > fd2[0] ? fd1[0] : fd2[0];
 
         int ret = select(maxfd + 1, &readfds, NULL, NULL, NULL);
         if (ret < 0) {
@@ -50,19 +57,28 @@ int main(void) {
 
         if (FD_ISSET(STDIN_FILENO, &readfds)) {
             ssize_t n = read(STDIN_FILENO, buff, sizeof(buff));
-         
+            if (n > 0) {
                 write(STDOUT_FILENO, "STDIN: ", 7);
                 write(STDOUT_FILENO, buff, n);
-            
+            }
         }
 
-        if (FD_ISSET(fd[0], &readfds)) {
-            ssize_t n = read(fd[0], buff, sizeof(buff));
-            
-                write(STDOUT_FILENO, "PIPE: ", 6);
+        if (FD_ISSET(fd1[0], &readfds)) {
+            ssize_t n = read(fd1[0], buff, sizeof(buff));
+            if (n > 0) {
+                write(STDOUT_FILENO, "C1: ", 4);
                 write(STDOUT_FILENO, buff, n);
-         }
+            }
         }
+
+        if (FD_ISSET(fd2[0], &readfds)) {
+            ssize_t n = read(fd2[0], buff, sizeof(buff));
+            if (n > 0) {
+                write(STDOUT_FILENO, "C2: ", 4);
+                write(STDOUT_FILENO, buff, n);
+            }
+        }
+    }
 
     return 0;
 }
