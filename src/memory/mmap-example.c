ef82c3d42c05dc651e0321a7ad2839e5ae6171d9
Saransivakumar89 <saransivakumar89@gmail.com>
2026-04-27 20:47:51 +0530

    feat(mmap): add the mmap example

---
+++ src/memory/mmap-example.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<sys/mman.h>
+
+int main(void){
+    
+    int * ptr = mmap(NULL, 4, PROT_READ | PROT_WRITE , MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if(ptr == MAP_FAILED){
+        perror("mmap");
+        return -1;
+    }
+
+    ptr[0] = 43;
+
+    printf("*ptr = %d\n", *ptr);
+
+    if(munmap(ptr,4)==-1){
+        perror("munmap");
+        return -1;
+    }
+
+    return 0 ;
+}
+
