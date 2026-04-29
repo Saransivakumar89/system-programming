185847c79bbbf86c808db1282791b35e58a9eff2
Saransivakumar89 <saransivakumar89@gmail.com>
2026-04-29 20:30:18 +0530

    feat(thread): implemented the t2

--- src/threads/thread-demo.c
+++ src/threads/thread-demo.c
@@ -3,20 +3,35 @@
 #include<unistd.h>
 #include<pthread.h>
 
+/* 
+ * output-interleave will happen because there is no synchronization
+ */
+
 void *task(void * arg){
     while(1){
-        printf("Thread running...\n");
+        printf("Thread-1 running...\n");
         sleep(2);   
+    }
+}
+
+void *new_task(void *arg){
+    while(1){
+        printf("Thread-2 running...\n");
+        sleep(2);
     }
 }
 
 int main(void){
     
-    pthread_t t1;
+    pthread_t t1, t2;
 
     pthread_create(&t1,NULL,task,NULL);
 
+    pthread_create(&t2,NULL,new_task,NULL);
+
     pthread_join(t1,NULL);
+    
+    pthread_join(t2,NULL);
 
     return 0 ;
 }
