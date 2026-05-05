211029b1f2bc45c7de028a3ce74a1afa2f48595f
Saransivakumar89 <saransivakumar89@gmail.com>
2026-05-05 21:09:32 +0530

    feat: add the return val from thread worker

--- src/threads/101-thread.c
+++ src/threads/101-thread.c
@@ -1,25 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<pthread.h>
 
 void *task_A(void * arg){
     printf("Thread 1 : A\n");
-    return NULL;
+    int *x = malloc(sizeof(int));
+    *x = 12;
+    pthread_exit(x);
 }
 
 void * task_B(void * arg){
     printf("Thread 2 : B\n");
-    return NULL;
+    int * y = malloc(sizeof(int));
+    *y = 21;
+    pthread_exit(y);
 }
 
 int main(void){
     
     pthread_t t1,t2;
 
+    void *x;
+    void *y;
+
     pthread_create(&t1, NULL, task_A, NULL);
     pthread_create(&t2, NULL, task_B, NULL);
     
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+    pthread_join(t1, &x);
+    pthread_join(t2, &y);
+
+    printf("returned value t1 : %d, t2 : %d\n", *(int*)x, *(int*)y);
 
     return 0 ;
 }
