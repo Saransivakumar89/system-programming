70df7746867d266b81548277e44d59c09f0e0e91
Saransivakumar89 <saransivakumar89@gmail.com>
2026-05-08 17:46:06 +0530

    feat: add the thread id

--- src/threads/broad-cast.c
+++ src/threads/broad-cast.c
@@ -8,13 +8,14 @@ int ready = 0 ;
 
 void * worker(void * arg)
 {
+    int id = *(int *)arg;
     pthread_mutex_lock(&lock);
     
     while(!ready){
         pthread_cond_wait(&cond, &lock);
     }
 
-    printf("Received the broadcast signal...\n");
+    printf("Received the broadcast signal from %d...\n", id);
     
 
     pthread_mutex_unlock(&lock);
@@ -25,21 +26,21 @@ void * worker(void * arg)
 int main(void){
     
     pthread_t t1, t2;
-
+    int id1 = 1, id2 = 2;
     pthread_mutex_init(&lock, NULL);
-    //pthread_cond_init(&cond, NULL);
+    pthread_cond_init(&cond, NULL);
 
-    pthread_create(&t1, NULL, worker, NULL);
-   // pthread_create(&t2, NULL, worker, NULL);
+    pthread_create(&t1, NULL, worker, &id1);
+   pthread_create(&t2, NULL, worker, &id2);
     
-    pthread_mutex_lock(&lock);
+   // pthread_mutex_lock(&lock);
     sleep(2);
     ready = 1;
     pthread_cond_broadcast(&cond);
-    pthread_mutex_unlock(&lock);
+   // pthread_mutex_unlock(&lock);
 
     pthread_join(t1,NULL);
-   // pthread_join(t2,NULL);
+    pthread_join(t2,NULL);
     
     pthread_cond_destroy(&cond);
     pthread_mutex_destroy(&lock);
