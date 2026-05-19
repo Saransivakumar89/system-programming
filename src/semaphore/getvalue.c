d65e0a860e78d36fcc418975f61b203e9657e8a9
Saransivakumar89 <saransivakumar89@gmail.com>
2026-05-19 21:38:58 +0530

    feat: add the get value

--- src/semaphore/getvalue.c
+++ src/semaphore/getvalue.c
@@ -1,32 +1,45 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<pthread.h>
-#include<semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-int main(void){
-    
-    sem_t sem;
+sem_t sem;
 
+void *worker(void *arg)
+{
     int val;
 
-    sem_init(&sem, 0 , 3);
-
+    /* Acquire semaphore */
     sem_wait(&sem);
-    
+
     sem_getvalue(&sem, &val);
+    printf("Entered critical section : %d\n", val);
 
-    printf("After sem wait : %d\n", val);
+    sleep(3);
 
-    sleep(2);
-
+    /* Release semaphore */
     sem_post(&sem);
 
-    sem_getvalue(&sem,&val);
-    
-    printf("After the sem post : %d\n", val);
+    sem_getvalue(&sem, &val);
+    printf("Exited critical section : %d\n", val);
+
+    pthread_exit(NULL);
+}
+
+int main(void)
+{
+    pthread_t t1, t2;
+
+    /* Initialize semaphore with value 1 */
+    sem_init(&sem, 0, 1);
+
+    pthread_create(&t1, NULL, worker, NULL);
+    pthread_create(&t2, NULL, worker, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
 
     sem_destroy(&sem);
 
-    return 0 ;
+    return 0;
 }
-
